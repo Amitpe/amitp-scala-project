dfa2c67442cd9e5bb31dc0ff53749aa5d5b12662
amitp <amitp@wix.com>
2024-08-24 15:48:09 +0300

    refactor FirewallParser

--- src/main/scala/common/FirewallParser.scala
+++ src/main/scala/common/FirewallParser.scala
@@ -1,51 +1,60 @@
 package common
 
 import api.LogEntry
-
-import scala.util.matching.Regex
+import common.FirewallParser._
 
 trait Parser {
   def parseLogLine(line: String): Option[LogEntry]
 }
 
 class FirewallParser extends Parser {
-  def parseLogLine(line: String): Option[LogEntry] = {
-    // Ensure the line contains 'kernel'
-    if (!line.contains("kernel")) return None
 
-    // Find the part of the line starting after 'kernel'
-    val kernelIndex = line.indexOf("kernel") + "kernel: ".length
-    val restOfLine = line.substring(kernelIndex)
+  override def parseLogLine(line: String): Option[LogEntry] = {
+    val direction: String = determineDirection(line)
+    val parts: Map[String, String] = splitToKeyValuePairs(line)
 
-    // Find the direction (INBOUND or OUTG CONN)
-    val direction = if (restOfLine.contains("INBOUND")) "INBOUND"
-    else if (restOfLine.contains("OUTG CONN")) "OUTG"
+    val userIp = extractUserIP(direction, parts)
+    val cloudIp = extractCloudIP(direction, parts)
+
+    // Extract optional fields
+    val domain = parts.get(DOMAIN)
+    val user = parts.get(USER)
+
+    buildLogEntry(userIp, cloudIp, domain, user)
+  }
+
+  private def determineDirection(line: String) = {
+    if (line.contains(INBOUND)) INBOUND
+    else if (line.contains(OUTBOUND)) OUTBOUND
     else ""
+  }
 
-    // Split the rest of the line by spaces to extract IP and domain
-    val parts = restOfLine.split(" ")
+  private def splitToKeyValuePairs(line: String): Map[String, String] = {
+    line.split("\\s+").collect {
+      case part if part.contains("=") => part.split("=") match {
+        case Array(key, value) => key -> value
+      }
+    }.toMap
+  }
 
-    // Extract IP based on direction
-    val (userIp, cloudIp) = direction match {
-      case "INBOUND" =>
-        (
-          parts.find(_.startsWith("DST=")).map(_.split("=")(1)),
-          parts.find(_.startsWith("SRC=")).map(_.split("=")(1))
-        )
-      case "OUTG" =>
-        (
-          parts.find(_.startsWith("SRC=")).map(_.split("=")(1)),
-          parts.find(_.startsWith("DST=")).map(_.split("=")(1)),
-        )
-      case _ => (None, None)
+  private def extractUserIP(direction: String, parts: Map[String, String]): Option[String] = {
+    direction match {
+      case INBOUND => parts.get(DESTINATION)
+      case OUTBOUND => parts.get(SOURCE)
+      case _ => None
     }
+  }
 
-    // Extract domain if present
-    val domain = parts.find(_.startsWith("DOMAIN=")).map(_.split("=")(1))
+  private def extractCloudIP(direction: String, parts: Map[String, String]): Option[String] = {
+    direction match {
+      case INBOUND => parts.get(SOURCE)
+      case OUTBOUND => parts.get(DESTINATION)
+      case _ => None
+    }
+  }
 
-    // Extract user if present
-    val user = parts.find(_.startsWith("USER=")).map(_.split("=")(1))
-
+  private def buildLogEntry(userIp: Option[String], cloudIp: Option[String], domain: Option[String], user: Option[String]) = {
+    // Ensuring both userIp and cloudIp exist, otherwise return None
     (userIp, cloudIp) match {
       case (Some(userIp), Some(cloudIp)) =>
         Some(LogEntry(userIp, cloudIp, domain, user))
@@ -53,5 +62,13 @@ class FirewallParser extends Parser {
         None
     }
   }
+}
 
+object FirewallParser {
+  private final val INBOUND = "INBOUND"
+  private final val OUTBOUND = "OUTG CONN"
+  private final val DESTINATION = "DST"
+  private final val SOURCE = "SRC"
+  private final val DOMAIN = "DOMAIN"
+  private final val USER = "USER"
 }
